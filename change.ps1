e91935c975f4c7c86c96531e9d515bebe02cec9e
khensler <46349570+khensler@users.noreply.github.com>
2020-01-10 08:38:23 -0500

    Update change.ps1

--- change.ps1
+++ change.ps1
@@ -1,6 +1,6 @@
 
 $vms = Get-VM | Get-NetworkAdapter | Select @{N='VM';E={$_.Parent.Name}},@{N='AdapterName';E={$_.Name}},@{N='Type';E={$_.Type}} | Where-Object{$_.Type -ne "Vmxnet3"}
-foreach $vm in $vms{
+foreach ($vm in $vms){
 	$vm | shutdown-vmguest -confirm:$false
 	$vm | Get-NetworkAdapter | set-networkadapter -type vmxnet3 -confirm:$false
 	$vm | start-vm -confirm:$false
