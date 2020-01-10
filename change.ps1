a7077d7d916f6fb0ff73ea7a46baeb5b0d183827
khensler <46349570+khensler@users.noreply.github.com>
2020-01-10 08:41:12 -0500

    Update change.ps1

--- change.ps1
+++ change.ps1
@@ -2,6 +2,10 @@
 $vms = Get-VM | Get-NetworkAdapter | Select @{N='VM';E={$_.Parent.Name}},@{N='AdapterName';E={$_.Name}},@{N='Type';E={$_.Type}} | Where-Object{$_.Type -ne "Vmxnet3"}
 foreach ($vm in $vms){
 	$vm | shutdown-vmguest -confirm:$false
+	while($vm.PowerState -eq 'PoweredOn'){
+		sleep 5
+		$vm = Get-VM -Name $vm.name
+	}
 	$vm | Get-NetworkAdapter | set-networkadapter -type vmxnet3 -confirm:$false
 	$vm | start-vm -confirm:$false
 }
