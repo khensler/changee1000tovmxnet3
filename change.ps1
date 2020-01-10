d9a7e11c7c9b165081c3eac479ca99c42e867c6a
khensler <46349570+khensler@users.noreply.github.com>
2020-01-10 08:36:23 -0500

    Update change.ps1
    changed to look for nic type != vmxnet3

--- change.ps1
+++ change.ps1
@@ -1,5 +1,5 @@
 
-$vms = Get-VM | Get-NetworkAdapter | Select @{N='VM';E={$_.Parent.Name}},@{N='AdapterName';E={$_.Name}},@{N='Type';E={$_.Type}} | Where-Object{$_.Type -eq "e1000"}
+$vms = Get-VM | Get-NetworkAdapter | Select @{N='VM';E={$_.Parent.Name}},@{N='AdapterName';E={$_.Name}},@{N='Type';E={$_.Type}} | Where-Object{$_.Type -ne "Vmxnet3"}
 foreach $vm in $vms{
 	$vm | shutdown-vmguest -confirm:$false
 	$vm | Get-NetworkAdapter | set-networkadapter -type vmxnet3 -confirm:$false
